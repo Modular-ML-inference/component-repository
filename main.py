6a27792d347d705ae1b066b32fe62c82bf53f3e1
kbogacka <kbogacka766@gmail.com>
2022-02-20 21:43:12 +0100

    Add MongoDB support

--- main.py
+++ main.py
@@ -1,168 +1,254 @@
-from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel, Field
+from http import HTTPStatus
+
+import gridfs
+import uvicorn
+from bson import ObjectId
+from fastapi import FastAPI, status, UploadFile, File, Response, HTTPException
+from pymongo import MongoClient
+from starlette.responses import StreamingResponse
+
+from config import HOST, DB_PORT, PORT
+from pydloc.models import MLModel, MLAlgorithm, MLCollector, MLModelData, MLAlgorithmData, MLCollectorData
 
 app = FastAPI()
 
-# ---------------- models ---------------- #
-class MLModelData(BaseModel):
-    data: str = Field(None, title="model binary data encoded as base64 string")
-    meta: dict[str,str] = Field(None, title="model metadata as key-value pairs")
-
-class MLModel(MLModelData):
-    id: int = Field(None, title="model identified, numeric")
-    version: int = Field(None, title="model version, numeric")
-
-models = {}
-models['1|1']=MLModel(id=1,version=1,data='VGhlIEZpcnN0IEVydXB0aW9u',meta={'a':'1','b':'2'})
-models['1|2']=MLModel(id=1,version=2,data='Uml0ZSBvZiBCZWx6ZW5sb2s=',meta={'a':'3','b':'4'})
-models['2|1']=MLModel(id=2,version=1,data='T3V0Zmxhbms=',meta={'a':'5','b':'6'})
-models['3|1']=MLModel(id=3,version=1,data='Q2hhcm1pbmcgUHJpbmNl',meta={'a':'7','b':'8'})
-
-
-# ---------------- algortihms ---------------- #
-class MLAlgorithmData(BaseModel):
-    data: str = Field(None, title="algorithm binary data encoded as base64 string")
-    meta: dict[str,str] = Field(None, title="algorithm metadata as key-value pairs")
-
-class MLAlgorithm(MLAlgorithmData):
-    name: str = Field(None, title="algorithm name")
-    version: int = Field(None, title="algorithm version, numeric")
-
-algorithms = {}
-algorithms['algo_1|1']=MLAlgorithm(name='algo_1',version=1,data='VGhlIEZpcnN0IEVydXB0aW9u',meta={'a':'1','b':'2'})
-algorithms['algo_1|2']=MLAlgorithm(name='algo_1',version=2,data='Uml0ZSBvZiBCZWx6ZW5sb2s=',meta={'a':'3','b':'4'})
-algorithms['algo_2|1']=MLAlgorithm(name='algo_2',version=1,data='T3V0Zmxhbms=',meta={'a':'5','b':'6'})
-algorithms['algo_3|1']=MLAlgorithm(name='algo_3',version=1,data='Q2hhcm1pbmcgUHJpbmNl',meta={'a':'7','b':'8'})
-
-
-# ---------------- collectors ---------------- #
-class MLCollectorData(BaseModel):
-    data: str = Field(None, title="collector binary data encoded as base64 string")
-    meta: dict[str,str] = Field(None, title="collector metadata as key-value pairs")
-
-class MLCollector(MLCollectorData):
-    name: str = Field(None, title="collector name")
-    version: int = Field(None, title="collector version, numeric")
-
-collectors = {}
-collectors['coll_1|1']=MLCollector(name='coll_1',version=1,data='VGhlIEZpcnN0IEVydXB0aW9u',meta={'a':'1','b':'2'})
-collectors['coll_1|2']=MLCollector(name='coll_1',version=2,data='Uml0ZSBvZiBCZWx6ZW5sb2s=',meta={'a':'3','b':'4'})
-collectors['coll_2|1']=MLCollector(name='coll_2',version=1,data='T3V0Zmxhbms=',meta={'a':'5','b':'6'})
-collectors['coll_3|1']=MLCollector(name='coll_3',version=1,data='Q2hhcm1pbmcgUHJpbmNl',meta={'a':'7','b':'8'})
-
-
-# ---------------- models endpoint ---------------- #
-
-def create_model_id(id:int, ver:int):
-    return str(id) + "|" + str(ver)
 
 @app.post("/model", status_code=status.HTTP_201_CREATED)
-async def create_model(model:MLModel):
-    mid = create_model_id(model.id, model.version)
-    if mid in models:
-        raise HTTPException(status_code=400,detail="model already exists")
+async def create_model(model: MLModel):
+    db = client.repository
+    if len(list(db.models.find({'id': model.id, 'version': model.version}).limit(1))) > 0:
+        raise HTTPException(status_code=400, detail='Model with this id and version already in repository')
     else:
-        models[mid]=model
+        try:
+            db.models.insert_one(model.dict(by_alias=True))
+        except Exception as e:
+            print("An exception occurred ::", e)
+            raise HTTPException(status_code=500)
+
 
 @app.put("/model/{id}/{version}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_model(id:int, version:int, model:MLModelData):
-    mid = create_model_id(id, version)
-    if mid in models:
-        models[mid].data = model.data
-        models[mid].meta = model.meta
+async def update_model(id: int, version: int, file: UploadFile = File(...)):
+    db = client.repository
+    db_grid = client.repository_grid
+    fs = gridfs.GridFS(db_grid)
+    if len(list(db.models.find({'id': id, 'version': version}).limit(1))) > 0:
+        data = await file.read()
+        model_id = fs.put(data, filename=f'model/{id}/{version}')
+        db.models.update_one({'id': id, 'version': version}, {"$set": {"model_id": str(model_id)}},
+                             upsert=False)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
     else:
-        raise HTTPException(status_code=404,detail="model not found")
+        raise HTTPException(status_code=404, detail="model not found")
+
+
+@app.put("/model/meta/{id}/{version}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_model_meta(id: int, version: int, meta: MLModelData):
+    db = client.repository
+    if len(list(db.models.find({'id': id, 'version': version}).limit(1))) > 0:
+        db.models.update_one({'id': id, 'version': version}, {"$set": {"meta": dict(meta.meta)}},
+                             upsert=False)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
+    else:
+        raise HTTPException(status_code=404, detail="model not found")
+
 
 @app.get("/model")
 async def get_model_list():
-    return list(models.values())
+    db = client.repository
+    keys = list(MLModel.__fields__)
+    list_m = [{key: document.get(key) for key in keys} for document in db.models.find()]
+    return list_m
+
 
 @app.get("/model/{id}/{version}")
-async def get_model(id:int, version:int):
-    return models[create_model_id(id, version)]
+async def get_model(id: int, version: int):
+    db = client.repository
+    model_id = db.models.find_one({'id': id, 'version': version})['model_id']
+    db_grid = client.repository_grid
+    fs = gridfs.GridFSBucket(db_grid)
+    file_handler = fs.open_download_stream(ObjectId(model_id))
+
+    def read_gridfs():
+        eachline = file_handler.readline()
+        while eachline:
+            yield eachline
+            eachline = file_handler.readline()
+
+    return StreamingResponse(read_gridfs())
+
 
 @app.delete("/model/{id}/{version}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_model(id:int, version:int):
-    mid = create_model_id(id, version)
-    if mid in models:
-        models.pop(mid)
+async def delete_model(id: int, version: int):
+    db = client.repository
+    db_grid = client.repository_grid
+    fs = gridfs.GridFS(db_grid)
+    if len(list(db.models.find({'id': id, 'version': version}).limit(1))) > 0:
+        model_id = db.models.find_one({'id': id, 'version': version})['model_id']
+        db.models.delete_many({'id': id, 'version': version, 'model_id': model_id})
+        fs.delete(model_id)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
     else:
-        raise HTTPException(status_code=404,detail="model not found")
+        raise HTTPException(status_code=404, detail="model not found")
 
-
-# ---------------- algorithms endpoint ---------------- #
-
-def create_algo_id(name:str, ver:int):
-    return name + "|" + str(ver)
 
 @app.post("/algorithm", status_code=status.HTTP_201_CREATED)
-async def create_algorithm(algorithm:MLAlgorithm):
-    aid = create_algo_id(algorithm.name, algorithm.version)
-    if aid in algorithms:
-        raise HTTPException(status_code=400,detail="algorithm already exists")
+async def create_algorithm(algorithm: MLAlgorithm):
+    db = client.repository
+    if len(list(db.algorithms.find({'name': algorithm.name, 'version': algorithm.version}).limit(1))) > 0:
+        raise HTTPException(status_code=400, detail='Algorithm already exists')
     else:
-        algorithms[aid]=algorithm
+        try:
+            db.algorithms.insert_one(algorithm.dict(by_alias=True))
+        except Exception as e:
+            print("An exception occurred ::", e)
+            raise HTTPException(status_code=500)
+
 
 @app.put("/algorithm/{name}/{version}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_algorithm(name:str, version:int, model:MLAlgorithmData):
-    aid = create_algo_id(name, version)
-    if aid in algorithms:
-        algorithms[aid].data = model.data
-        algorithms[aid].meta = model.meta
+async def update_algorithm(name: str, version: int, file: UploadFile = File(...)):
+    db = client.repository
+    db_grid = client.repository_grid
+    fs = gridfs.GridFS(db_grid)
+    if len(list(db.algorithms.find({'name': name, 'version': version}).limit(1))) > 0:
+        data = await file.read()
+        algorithm_id = fs.put(data, filename=f'algorithm/{name}/{version}')
+        db.algorithms.update_one({'name': name, 'version': version},
+                                 {"$set": {"algorithm_id": str(algorithm_id)}},
+                                 upsert=False)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
     else:
-        raise HTTPException(status_code=404,detail="algorithm not found")
+        raise HTTPException(status_code=404, detail="algorithm not found")
+
+
+@app.put("/algorithm/meta/{name}/{version}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_algorithm_meta(name: str, version: int, meta: MLAlgorithmData):
+    db = client.repository
+    if len(list(db.algorithms.find({'name': name, 'version': version}).limit(1))) > 0:
+        db.algorithms.update_one({'name': name, 'version': version}, {"$set": {"meta": dict(meta.meta)}},
+                                 upsert=False)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
+    else:
+        raise HTTPException(status_code=404, detail="algorithm not found")
+
 
 @app.get("/algorithm")
 async def get_algorithm_list():
-    return list(algorithms.values())
+    db = client.repository
+    keys = list(MLAlgorithm.__fields__)
+    list_m = [{key: document.get(key) for key in keys} for document in db.algorithms.find()]
+    return list_m
+
 
 @app.get("/algorithm/{name}/{version}")
-async def get_algorithm(name:str, version:int):
-    return algorithms[create_algo_id(name, version)]
+async def get_algorithm(name: str, version: int):
+    db = client.repository
+    algorithm_id = db.algorithms.find_one({'name': name, 'version': version})['algorithm_id']
+    db_grid = client.repository_grid
+    fs = gridfs.GridFSBucket(db_grid)
+    file_handler = fs.open_download_stream(ObjectId(algorithm_id))
+
+    def read_gridfs():
+        eachline = file_handler.readline()
+        while eachline:
+            yield eachline
+            eachline = file_handler.readline()
+
+    return StreamingResponse(read_gridfs(), media_type="application/octet-stream")
+
 
 @app.delete("/algorithm/{name}/{version}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_algorithm(name:str, version:int):
-    aid = create_algo_id(name, version)
-    if aid in algorithms:
-        algorithms.pop(aid)
+async def delete_algorithm(name: str, version: int):
+    db = client.repository
+    db_grid = client.repository_grid
+    fs = gridfs.GridFS(db_grid)
+    if len(list(db.algorithms.find({'name': name, 'version': version}).limit(1))) > 0:
+        algorithm_id = db.algorithms.find_one({'name': name, 'version': version})['algorithm_id']
+        db.algorithms.delete_many({'name': name, 'version': version, 'algorithm_id': algorithm_id})
+        fs.delete(algorithm_id)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
     else:
-        raise HTTPException(status_code=404,detail="algorithm not found")
+        raise HTTPException(status_code=404, detail="model not found")
 
-
-# ---------------- collectors endpoint ---------------- #
-
-def create_coll_id(name:str, ver:int):
-    return name + "|" + str(ver)
 
 @app.post("/collector", status_code=status.HTTP_201_CREATED)
-async def create_collector(collector:MLCollector):
-    cid = create_coll_id(collector.name, collector.version)
-    if cid in collectors:
-        raise HTTPException(status_code=400,detail="collector already exists")
+async def create_collector(collector: MLCollector):
+    db = client.repository
+    if len(list(db.collectors.find({'name': collector.name, 'version': collector.version}).limit(1))) > 0:
+        raise HTTPException(status_code=400, detail='Collector already exists')
     else:
-        collectors[cid]=collector
+        try:
+            db.collectors.insert_one(collector.dict(by_alias=True))
+        except Exception as e:
+            print("An exception occurred ::", e)
+            raise HTTPException(status_code=500)
+
 
 @app.put("/collector/{name}/{version}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_collector(name:str, version:int, model:MLCollectorData):
-    cid = create_coll_id(name, version)
-    if cid in collectors:
-        collectors[cid].data = model.data
-        collectors[cid].meta = model.meta
+async def update_collector(name: str, version: int, file: UploadFile = File(...)):
+    db = client.repository
+    db_grid = client.repository_grid
+    fs = gridfs.GridFS(db_grid)
+    if len(list(db.collectors.find({'name': name, 'version': version}).limit(1))) > 0:
+        data = await file.read()
+        collector_id = fs.put(data, filename=f'collector/{name}/{version}')
+        db.collectors.update_one({'name': name, 'version': version},
+                                 {"$set": {"collector_id": str(collector_id)}},
+                                 upsert=False)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
     else:
-        raise HTTPException(status_code=404,detail="collector not found")
+        raise HTTPException(status_code=404, detail="collector not found")
+
+
+@app.put("/collector/meta/{name}/{version}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_collector_meta(name: str, version: int, meta: MLCollectorData):
+    db = client.repository
+    if len(list(db.collectors.find({'name': name, 'version': version}).limit(1))) > 0:
+        db.collectors.update_one({'name': name, 'version': version}, {"$set": {"meta": dict(meta.meta)}},
+                                 upsert=False)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
+    else:
+        raise HTTPException(status_code=404, detail="collector not found")
+
 
 @app.get("/collector")
 async def get_collector_list():
-    return list(collectors.values())
+    db = client.repository
+    keys = list(MLCollector.__fields__)
+    list_m = [{key: document.get(key) for key in keys} for document in db.collectors.find()]
+    return list_m
+
 
 @app.get("/collector/{name}/{version}")
-async def get_collector(name:str, version:int):
-    return collectors[create_coll_id(name, version)]
+async def get_collector(name: str, version: int):
+    db = client.repository
+    collector_id = db.collectors.find_one({'name': name, 'version': version})['collector_id']
+    db_grid = client.repository_grid
+    fs = gridfs.GridFSBucket(db_grid)
+    file_handler = fs.open_download_stream(ObjectId(collector_id))
+
+    def read_gridfs():
+        eachline = file_handler.readline()
+        while eachline:
+            yield eachline
+            eachline = file_handler.readline()
+
+    return StreamingResponse(read_gridfs(), media_type="application/octet-stream")
+
 
 @app.delete("/collector/{name}/{version}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_collector(name:str, version:int):
-    cid = create_coll_id(name, version)
-    if cid in collectors:
-        collectors.pop(aid)
+async def delete_collector(name: str, version: int):
+    db = client.repository
+    db_grid = client.repository_grid
+    fs = gridfs.GridFS(db_grid)
+    if len(list(db.collectors.find({'name': name, 'version': version}).limit(1))) > 0:
+        collector_id = db.collectors.find_one({'name': name, 'version': version})['collector_id']
+        db.collectors.delete_many({'name': name, 'version': version, 'collector_id': collector_id})
+        fs.delete(collector_id)
+        return Response(status_code=HTTPStatus.NO_CONTENT.value)
     else:
-        raise HTTPException(status_code=404,detail="collector not found")
+        raise HTTPException(status_code=404, detail="model not found")
+
+
+if __name__ == "__main__":
+    with MongoClient(port=DB_PORT) as client:
+        uvicorn.run(app, host=HOST, port=PORT)
---
+++ pydloc/models.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+from pydantic import BaseModel, Field
+
+
+class MLModelData(BaseModel):
+    meta: Dict[str, str] = Field(None, title="model metadata as key-value pairs")
+
+
+class MLModel(MLModelData):
+    id: int = Field(None, title="model identified, numeric")
+    version: int = Field(None, title="model version, numeric")
+    model_id: str = Field(None, title="id under which model is stored in gridfs")
+
+
+class MLAlgorithmData(BaseModel):
+    meta: Dict[str, str] = Field(None, title="algorithm metadata as key-value pairs")
+
+
+class MLAlgorithm(MLAlgorithmData):
+    name: str = Field(None, title="algorithm name")
+    version: int = Field(None, title="algorithm version, numeric")
+    algorithm_id: str = Field(None, title="id under which algorithm is stored in gridfs")
+
+
+class MLCollectorData(BaseModel):
+    meta: Dict[str, str] = Field(None, title="collector metadata as key-value pairs")
+
+
+class MLCollector(MLCollectorData):
+    name: str = Field(None, title="collector name")
+    version: int = Field(None, title="collector version, numeric")
+    collector_id: str = Field(None, title="id under which collector is stored in gridfs")

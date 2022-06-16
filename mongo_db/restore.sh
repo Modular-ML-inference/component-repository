a278e1970b2bddadafeb9b1d3a1090ca12d634a5
Karolina-Bogacka <karolina.bogacka@skygate.io>
2022-06-16 10:12:35 +0200

    Add some testing data under model name and versio base/base2

---
+++ mongo_db/restore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Restore from dump
+mongorestore --db repository --collection models /dump/repository/models.bson
+mongorestore --db repository_grid --collection fs.chunks /dump/repository_grid/fs.chunks.bson
+mongorestore --db repository_grid --collection fs.files /dump/repository_grid/fs.files.bson

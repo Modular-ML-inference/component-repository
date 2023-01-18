63a1ed298532594f642c35a1ad91ee556c44f9e8
kbogacka <kbogacka766@gmail.com>
2023-01-18 16:52:29 +0000

    local changes

--- application/config.py
+++ application/config.py
@@ -2,5 +2,5 @@ HOST = "0.0.0.0"
 PORT = 9012
 DB_PORT = 27017
 SERVER_PORT = 8000
-# DB_ADDRESS = "flrepository-repositorydb-svc1.default.svc.cluster.local" for kubernetes
-DB_ADDRESS = "repositorydb"
+DB_ADDRESS = "flrepository-repositorydb-svc1.default.svc.cluster.local"
+#DB_ADDRESS = "repositorydb"

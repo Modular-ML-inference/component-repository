666610aa3ddc248b2381f41fa19156ba475afcb1
Karolina Bogacka <bogacka@ibspan.waw.pl>
2023-03-30 11:07:57 +0200

    Add ConfigMaps instead of config to repository

--- application/database.py
+++ application/database.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
 
 from application.config import DB_PORT, DB_ADDRESS
+import logging
 
-
+logger = logging.getLogger()
+# log all messages, debug and up
+logger.setLevel(logging.INFO)
+logging.log(logging.INFO, f'{DB_ADDRESS}')
 client = MongoClient(DB_ADDRESS, int(DB_PORT))
